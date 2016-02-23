e96b7fde4e0d2458a1413d4f154b56c906182730
Austin Ducworth <aducworth@gmail.com>
2016-02-23 16:33:36 -0500

    added user id to activities

---
+++ database/migrations/2016_02_23_213238_add_user_id_to_activities.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserIdToActivities extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('activities', function (Blueprint $table) {
+            $table->integer('user_id')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('activities', function (Blueprint $table) {
+            $table->dropColumn('user_id');
+        });
+    }
+}
